54f0f6e2ae9e75be6e08e02f1e51850983215084
Leo Cai <leo.cai.coding@gmail.com>
2024-01-28 10:17:48 +1100

    test: trigger pipeline build

--- src/Routes/HealthCheck.router.ts
+++ src/Routes/HealthCheck.router.ts
@@ -2,6 +2,15 @@ import Router from "koa-router";
 
 const router = new Router();
 
+router.get('/', async (ctx) => {
+  try {
+    ctx.body = {
+      message: 'Welcome to Appoilution Notes API!'
+    }
+  } catch (e) {
+    console.error(e);
+  }
+});
 router.get('/ping', async (ctx) => {
   try {
     ctx.body = {
