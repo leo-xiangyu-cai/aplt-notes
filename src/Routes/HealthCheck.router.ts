d2accc012ccda8359eb713abdc853b053fc05262
Leo Cai <leo.cai.coding@gmail.com>
2024-01-29 23:07:26 +1100

    test: trigger pipeline build

--- src/Routes/HealthCheck.router.ts
+++ src/Routes/HealthCheck.router.ts
@@ -8,7 +8,7 @@ router.get('/', async (ctx) => {
 router.get('/ping', async (ctx) => {
   try {
     ctx.body = {
-      message: 'pong'
+      message: 'OK'
     }
   } catch (e) {
     console.error(e);
